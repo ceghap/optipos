3ac0777d022d80a6a962ffcf29aa73dd836210bf
Choo Jian Cheng <ext-choo.jian.cheng@solenovo.fi>
2023-06-17 16:57:18 +0800

    Redesigned pos page

--- src/components/SelectedItem.tsx
+++ src/components/SelectedItem.tsx
@@ -1,70 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import { Product } from "src/actions/product";
-
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import Image from 'next/image'
 
 const SelectedItem = ({ items, removeItem }: { items: Product[], removeItem: (id: number) => void }) => {
 	const [total, setTotal] = useState<number>(0)
 
 	useEffect(() => {
-		const totalPrice = items.reduce((accumulator, currentItem) => {
+		let totalPrice = items.reduce((accumulator, currentItem) => {
 			return accumulator + currentItem.price;
 		}, 0);
 
 		setTotal(Number(totalPrice.toFixed(2)))
 	}, [items])
+
+	let uniqueItems = [...new Set(items)];
+
 	return (
-		<>
 		<div className='flex flex-col justify-between h-full px-2'>
 			<div className='overflow-auto h-[calc(100vh-60px)] px-2' >
-				<ul className="flex flex-col divide-y divide-gray-700">
-					{items?.length === 0 && <p className='mb-4'>No product selected</p>}
-					{items.map((i: Product) => <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
-						<div className="flex w-full space-x-2 sm:space-x-4">
-							<img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" src={i.image} alt="Polaroid camera" />
-							<div className="flex flex-col justify-between w-full pb-4">
-								<div className="flex justify-between w-full pb-2 space-x-2">
-									<div className="space-y-1">
-										<h3 className="text-lg font-semibold leadi sm:pr-8">{i.title}</h3>
-									</div>
-									<div className="text-right">
-										<p className="text-lg font-semibold">RM {i.price}</p>
-									</div>
-								</div>
-								<div className="flex text-sm divide-x">
-									<button type="button" className="flex items-center px-2 py-1 pl-0 space-x-1" onClick={() => removeItem(i.id)}>
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-											<path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
-											<rect width="32" height="200" x="168" y="216"></rect>
-											<rect width="32" height="200" x="240" y="216"></rect>
-											<rect width="32" height="200" x="312" y="216"></rect>
-											<path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
-										</svg>
-										<span>Remove</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</li>)}
-
-				</ul>
+				{uniqueItems.length === 0 ? <p className='mb-4'>No product selected</p> :
+					uniqueItems.map((i: Product) => (
+						<Card key={i.id} className="relative flex flex-col">
+							<CardHeader className="p-2 pb-0 flex-grow">
+								<Image
+									src={i.image}
+									alt={i.title}
+									width={100}
+									height={100}
+								/>
+								<span className="absolute top-0 right-0 bg-red-500 text-white rounded-full h-6 w-6 text-center">
+									x{items.filter(item => item.id === i.id).length}
+								</span>
+							</CardHeader>
+							<CardContent className="p-2 flex-grow">
+								<CardTitle className="text-lg font-semibold leading-6 sm:pr-8">{i.title}</CardTitle>
+								<CardDescription className="text-lg font-semibold">RM {i.price}</CardDescription>
+							</CardContent>
+							<CardFooter className="p-2 pt-0 border-t border-gray-200 bg-gray-50">
+								<Button
+									className="w-full py-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+									onClick={() => removeItem(i.id)}
+								>
+									Remove
+								</Button>
+							</CardFooter>
+						</Card>
+					))}
 			</div>
-			<div>
-				<div className="space-y-1 text-right">
-					<p>Total amount:
-						<span className="font-semibold">RM {total}</span>
-					</p>
-
-				</div>
-				<div className="flex justify-end space-x-4">
-
-					<button type="button" className="px-6 py-2 border rounded-md bg-green-500 w-full text-white">
-						<span className="sr-only sm:not-sr-only">Pay</span>
-					</button>
-				</div>
+			<div className="flex flex-col space-x-4 ">
+				<p className="ml-4 mb-4">Total amount:
+					<span className="font-semibold">RM {total}</span>
+				</p>
+				<Button
+					className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+				>
+					Pay
+				</Button>
 			</div>
 		</div>
-
-		</>
 	)
 }
 
--- src/pages/pos/index.tsx
+++ src/pages/pos/index.tsx
@@ -1,19 +1,17 @@
 import SelectedItem from "@/components/SelectedItem";
 import PosLayout from "@/components/layouts/PosLayout";
 import { useQuery } from "@tanstack/react-query";
-import { Hand } from "lucide-react";
 import { useState } from 'react'
-import { fetchProducts } from "src/actions/product";
-import { Product } from "src/actions/product";
+import { fetchProducts, Product } from "src/actions/product";
+import Image from 'next/image'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card";
+import { Button } from "../../components/ui/button";
 
 const PosIndex = () => {
 
-  
   const [selectedItem, setSelectedItem] = useState<Product[]>([])
   const [searchValue, setSearchValue] = useState<string>('')
-  
   const { data } = useQuery({ queryKey: ['products', searchValue], queryFn: () => fetchProducts(searchValue) })
-
   const onAddItem = (id: number) => {
 
     const item = data?.find(d => d.id === id);
@@ -26,44 +24,69 @@ const PosIndex = () => {
 
   }
 
-  const removeItem = (id:number) => {
-
-    const newItem= selectedItem.filter((item: Product) => item.id !== id);
-
-    setSelectedItem(newItem)
-  }
-
-
+  const removeItem = (id: number) => {
+    const indexToRemove = selectedItem.findIndex(item => item.id === id);
+    if (indexToRemove > -1) {
+      setSelectedItem([
+        ...selectedItem.slice(0, indexToRemove),
+        ...selectedItem.slice(indexToRemove + 1),
+      ]);
+    }
+  };
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(event.target.value);
   };
 
   return (
     <PosLayout>
-      <div>
-        <div className="p-2 ">
-          <input className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" placeholder="Search..." onChange={handleSearchChange}/>
-        </div>
-        <div className="flex flex-row flex-nowrap justify-start content-stretch ">
-          <div className="bg-gray-100 w-4/6 overflow-auto h-[calc(100vh-60px)] p-4 flex flex-wrap justify-between space-y-4">
-
-            {data && data.map((d: Product) =>
-              <div key={d.id} className="w-52 bg-white p-4 inline-block">
-                <div className="w-full h-auto flex justify-center items-center">
-                  <img src={d.image} className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" />
-                </div>
-                <p className="w-48 text-ellipsis overflow-hidden whitespace-nowrap h-6">{d.title}</p>
-                <p className="font-bold text-lg">RM {d.price}</p>
-                <button className="px-8 py-3 w-full bg-yellow-400 font-semibold rounded dark:bg-gray-100 dark:text-gray-800" onClick={() => onAddItem(d.id)}>Add</button>
-              </div>
+      <div className="container mx-auto p-4 grid grid-cols-4 gap-4">
+        <div className="col-span-3">
+          <div className="w-full mb-4">
+            <input
+              className="w-full p-2 border-2 border-gray-300 bg-white h-8 px-4 pr-12 rounded-lg text-sm focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Search for products..."
+              onChange={handleSearchChange}
+            />
+          </div>
+          <div className="w-full grid grid-cols-3 gap-4">
+            {data && data?.map((d: Product) =>
+              <Card key={d.id} className="flex flex-col">
+                <CardHeader className="p-2 pb-0 flex-grow">
+                  <Image
+                    src={d.image}
+                    alt={d.title}
+                    width={100}
+                    height={100}
+                  />
+                </CardHeader>
+                <CardContent className="p-2 flex-grow">
+                  <CardTitle className="text-base">{d.title}</CardTitle>
+                  <CardDescription className="text-sm">RM {d.price}</CardDescription>
+                </CardContent>
+                <CardFooter className="p-2 pt-0 border-t border-gray-200 bg-gray-50">
+                  <Button
+                    className="w-full py-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={() => onAddItem(d.id)}
+                  >
+                    Add
+                  </Button>
+                </CardFooter>
+              </Card>
             )}
-
-          </div>
-          <div className="w-2/6 h-[calc(100vh-60px)] overflow-auto">
-            <SelectedItem items={selectedItem} removeItem={removeItem} />
           </div>
         </div>
-
+        <div className="col-span-1">
+          <SelectedItem items={selectedItem} removeItem={removeItem} />
+          <div className="sticky bottom-0 p-4">
+            {/* <Button
+              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+            >
+              Pay
+            </Button> */}
+          </div>
+        </div>
       </div>
     </PosLayout>
   );
