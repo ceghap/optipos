8f46b3bc439b6cf362fbcf3c70b05f72e0777b31
Ashraf Latif <hello@ceghap.com>
2023-06-17 19:22:49 +0800

    fix add sale

--- src/pages/api/sales/index.ts
+++ src/pages/api/sales/index.ts
@@ -1,24 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../prisma/client";
+import { Product } from "@prisma/client";
 
+interface ProductWithQuantity extends Product {
+    quantity: number;
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
     }
 
-    const { selectedItems } = req.body;
+    const { selectedItems }: { selectedItems: Product[] } = req.body;
 
     const totalPrice = selectedItems?.reduce((accumulator: number, currentItem: any) => {
         return accumulator + currentItem.price;
     }, 0);
+
+    const reducedSelectedItems: ProductWithQuantity[] = selectedItems.reduce((acc: any[], item) => {
+        const existingItem = acc.find((i) => i.id === item.id);
+
+        if (existingItem) {
+            existingItem.quantity += 1;
+        } else {
+            acc.push({ ...item, quantity: 1 });
+        }
+
+        return acc;
+    }, []);
 
     try {
         const sale = await prisma.sale.create({
             data: {
                 total: totalPrice,
                 items: {
-                    create: selectedItems.map((item: any) => ({
+                    create: reducedSelectedItems.map((item: ProductWithQuantity) => ({
                         quantity: item.quantity,
                         productId: item.id
                     }))
@@ -31,7 +47,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         // Deduct product stock count
         await Promise.all(
-            selectedItems.map((item: any) =>
+            reducedSelectedItems.map((item: ProductWithQuantity) =>
                 prisma.product.update({
                     where: { id: item.id },
                     data: { stockCount: { decrement: item.quantity } }
