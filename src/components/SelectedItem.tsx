cb209cd2793a77bbf7ec80940f2838eaec23897d
Ashraf Latif <hello@ceghap.com>
2023-06-17 15:32:45 +0800

    fix ui

--- src/components/SelectedItem.tsx
+++ src/components/SelectedItem.tsx
@@ -14,49 +14,56 @@ const SelectedItem = ({ items, removeItem }: { items: Product[], removeItem: (id
 	}, [items])
 	return (
 		<>
-			<ul className="flex flex-col divide-y divide-gray-700">
-				{items.map((i: Product) => <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
-					<div className="flex w-full space-x-2 sm:space-x-4">
-						<img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" src={i.image} alt="Polaroid camera" />
-						<div className="flex flex-col justify-between w-full pb-4">
-							<div className="flex justify-between w-full pb-2 space-x-2">
-								<div className="space-y-1">
-									<h3 className="text-lg font-semibold leadi sm:pr-8">{i.title}</h3>
-									<p className="text-sm dark:text-gray-400">Classic</p>
+		<div className='flex flex-col justify-between h-full px-2'>
+			<div className='overflow-auto h-[calc(100vh-60px)] px-2' >
+				<ul className="flex flex-col divide-y divide-gray-700">
+					{items?.length === 0 && <p className='mb-4'>No product selected</p>}
+					{items.map((i: Product) => <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
+						<div className="flex w-full space-x-2 sm:space-x-4">
+							<img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" src={i.image} alt="Polaroid camera" />
+							<div className="flex flex-col justify-between w-full pb-4">
+								<div className="flex justify-between w-full pb-2 space-x-2">
+									<div className="space-y-1">
+										<h3 className="text-lg font-semibold leadi sm:pr-8">{i.title}</h3>
+									</div>
+									<div className="text-right">
+										<p className="text-lg font-semibold">RM {i.price}</p>
+									</div>
 								</div>
-								<div className="text-right">
-									<p className="text-lg font-semibold">RM {i.price}</p>
+								<div className="flex text-sm divide-x">
+									<button type="button" className="flex items-center px-2 py-1 pl-0 space-x-1" onClick={() => removeItem(i.id)}>
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
+											<path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
+											<rect width="32" height="200" x="168" y="216"></rect>
+											<rect width="32" height="200" x="240" y="216"></rect>
+											<rect width="32" height="200" x="312" y="216"></rect>
+											<path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
+										</svg>
+										<span>Remove</span>
+									</button>
 								</div>
-							</div>
-							<div className="flex text-sm divide-x">
-								<button type="button" className="flex items-center px-2 py-1 pl-0 space-x-1" onClick={() => removeItem(i.id)}>
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
-										<path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
-										<rect width="32" height="200" x="168" y="216"></rect>
-										<rect width="32" height="200" x="240" y="216"></rect>
-										<rect width="32" height="200" x="312" y="216"></rect>
-										<path d="M328,88V40c0-13.458-9.488-24-21.6-24H205.6C193.488,16,184,26.542,184,40V88H64v32H448V88ZM216,48h80V88H216Z"></path>
-									</svg>
-									<span>Remove</span>
-								</button>
 							</div>
 						</div>
-					</div>
-				</li>)}
+					</li>)}
 
-			</ul>
-			<div className="space-y-1 text-right">
-				<p>Total amount:
-					<span className="font-semibold">RM {total}</span>
-				</p>
-
+				</ul>
 			</div>
-			<div className="flex justify-end space-x-4">
+			<div>
+				<div className="space-y-1 text-right">
+					<p>Total amount:
+						<span className="font-semibold">RM {total}</span>
+					</p>
 
-				<button type="button" className="px-6 py-2 border rounded-md bg-green-500 w-full text-white">
-					<span className="sr-only sm:not-sr-only">Pay</span>
-				</button>
+				</div>
+				<div className="flex justify-end space-x-4">
+
+					<button type="button" className="px-6 py-2 border rounded-md bg-green-500 w-full text-white">
+						<span className="sr-only sm:not-sr-only">Pay</span>
+					</button>
+				</div>
 			</div>
+		</div>
+
 		</>
 	)
 }
--- src/pages/pos/index.tsx
+++ src/pages/pos/index.tsx
@@ -44,22 +44,22 @@ const PosIndex = () => {
         <div className="p-2 ">
           <input className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" placeholder="Search..." onChange={handleSearchChange}/>
         </div>
-        <div className="flex items-baseline flex-row flex-nowrap justify-start content-stretch ">
+        <div className="flex flex-row flex-nowrap justify-start content-stretch ">
           <div className="bg-gray-100 w-4/6 overflow-auto h-[calc(100vh-60px)] p-4 flex flex-wrap justify-between space-y-4">
 
             {data && data.map((d: Product) =>
-              <div key={d.id} className="w-52 bg-white p-4">
-                <div className="w-36 h-36 flex justify-center items-center">
-                  <img src={d.image} className="w-full h-full" />
+              <div key={d.id} className="w-52 bg-white p-4 inline-block">
+                <div className="w-full h-auto flex justify-center items-center">
+                  <img src={d.image} className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" />
                 </div>
-                <p className="text-ellipsis overflow-hidden w-52">{d.title}</p>
+                <p className="w-48 text-ellipsis overflow-hidden whitespace-nowrap h-6">{d.title}</p>
                 <p className="font-bold text-lg">RM {d.price}</p>
                 <button className="px-8 py-3 w-full bg-yellow-400 font-semibold rounded dark:bg-gray-100 dark:text-gray-800" onClick={() => onAddItem(d.id)}>Add</button>
               </div>
             )}
 
           </div>
-          <div className=" w-2/6 p-4 h-[calc(100vh-60px)] overflow-auto">
+          <div className="w-2/6 h-[calc(100vh-60px)] overflow-auto">
             <SelectedItem items={selectedItem} removeItem={removeItem} />
           </div>
         </div>
