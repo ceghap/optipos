cb9dcfe59e2962d7b766290f4594f81f58664fa4
Choo Jian Cheng <ext-choo.jian.cheng@solenovo.fi>
2023-06-17 17:05:18 +0800

    Added skeleton to indicate loading state

--- src/pages/pos/index.tsx
+++ src/pages/pos/index.tsx
@@ -6,12 +6,13 @@ import { fetchProducts, Product } from "src/actions/product";
 import Image from 'next/image'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card";
 import { Button } from "../../components/ui/button";
+import { Skeleton } from "../../components/ui/skeleton";
 
 const PosIndex = () => {
 
   const [selectedItem, setSelectedItem] = useState<Product[]>([])
   const [searchValue, setSearchValue] = useState<string>('')
-  const { data } = useQuery({ queryKey: ['products', searchValue], queryFn: () => fetchProducts(searchValue) })
+  const { data, isLoading } = useQuery({ queryKey: ['products', searchValue], queryFn: () => fetchProducts(searchValue) })
   const onAddItem = (id: number) => {
 
     const item = data?.find(d => d.id === id);
@@ -51,30 +52,34 @@ const PosIndex = () => {
             />
           </div>
           <div className="w-full grid grid-cols-3 gap-4">
-            {data && data?.map((d: Product) =>
-              <Card key={d.id} className="flex flex-col">
-                <CardHeader className="p-2 pb-0 flex-grow">
-                  <Image
-                    src={d.image}
-                    alt={d.title}
-                    width={100}
-                    height={100}
-                  />
-                </CardHeader>
-                <CardContent className="p-2 flex-grow">
-                  <CardTitle className="text-base">{d.title}</CardTitle>
-                  <CardDescription className="text-sm">RM {d.price}</CardDescription>
-                </CardContent>
-                <CardFooter className="p-2 pt-0 border-t border-gray-200 bg-gray-50">
-                  <Button
-                    className="w-full py-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    onClick={() => onAddItem(d.id)}
-                  >
-                    Add
-                  </Button>
-                </CardFooter>
-              </Card>
-            )}
+            {
+              isLoading ?
+                Array(10).fill(0).map((_, idx) => <Skeleton key={idx} className="h-32" />) : // display 10 skeletons while data is loading
+                data && data?.map((d: Product) =>
+                  <Card key={d.id} className="flex flex-col">
+                    <CardHeader className="p-2 pb-0 flex-grow">
+                      <Image
+                        src={d.image}
+                        alt={d.title}
+                        width={100}
+                        height={100}
+                      />
+                    </CardHeader>
+                    <CardContent className="p-2 flex-grow">
+                      <CardTitle className="text-base">{d.title}</CardTitle>
+                      <CardDescription className="text-sm">RM {d.price}</CardDescription>
+                    </CardContent>
+                    <CardFooter className="p-2 pt-0 border-t border-gray-200 bg-gray-50">
+                      <Button
+                        className="w-full py-1 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => onAddItem(d.id)}
+                      >
+                        Add
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                )
+            }
           </div>
         </div>
         <div className="col-span-1">
@@ -93,3 +98,5 @@ const PosIndex = () => {
 };
 
 export default PosIndex;
+
+
